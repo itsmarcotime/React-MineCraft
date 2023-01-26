96d5ee5469e763a46d40d213949cfe977d2ae227
Marco <patrickstar1281@yahoo.com>
2023-01-26 10:21:10 -0600

    cubes are now in the game

---
+++ src/hooks/useStore.js
@@ -0,0 +1,34 @@
+import create from 'zustand'
+import { nanoid } from 'nanoid'
+
+export const useStore = create((set) => ({
+    texture: 'dirt',
+    cubes: [
+        {
+            key: nanoid(),
+            pos: [10, 0.5, 10],
+            texture: 'dirt',
+        },
+        {
+            key: nanoid(),
+            pos: [1, 0.5, 0],
+            texture: 'glass',
+        }
+    ],
+    addCube: (x,y,z) => {
+        set((prev) => ({
+            cubes: [
+                ...prev.cubes,
+                {
+                    key: nanoid(),
+                    pos: [x, y, z],
+                    texture: prev.texture
+                }
+            ]
+        }))
+    },
+    removeCube: () => {},
+    setTexture: () => {},
+    saveWorld: () => {},
+    resetWorld: () => {},
+}))
